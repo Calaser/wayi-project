47282edd1b229c3948621ee7ea081a46336c5b17
Calasa <s33003030@gmail.com>
2024-09-06 08:01:06 +0800

    Full version
    1.Finish CRUD functions.
    2.Layout change.

--- app/page.js
+++ app/page.js
@@ -8,13 +8,11 @@ export default function Home() {
   const [name, setName] = useState("");
   const [description, setDescription] = useState("");
   const [currentData, setCurrentData] = useState([]);
+  const [isSubmitInvalidData, setIsSubmitInvalidData] = useState(false);
+  const [isHideCompletedTask, setIisHideCompletedTask] = useState(false);
 
-  async function submitHandler(e) {
-    e.preventDefault();
-
-    let response = await axios.post("https://wayi.league-funny.com/api/task?page=1", { name: name, description: description, isCompleted: false });
-    console.log(response);
-    fetchData();
+  function showCompletedTaskHandler() {
+    setIisHideCompletedTask(prev => !prev);
   }
 
   async function fetchData() {
@@ -25,9 +23,53 @@ export default function Home() {
       let dataCount = response.data.total;
       for (let i = 1; dataCount > 10 * i; i++) {
         response = await axios.get(`https://wayi.league-funny.com/api/task?page=${i + 1}`);
-        dataArray = [...dataArray, ...response.data.data]
+        dataArray = [...dataArray, ...response.data.data];
       }
       setCurrentData(dataArray);
+    } catch (error) {
+      console.log("Error fetch:", error);
+    }
+  }
+
+  async function submitHandler(e) {
+    e.preventDefault();
+    try {
+      if (name.length > 10 || name.length < 1 || description.length > 30) {
+        setIsSubmitInvalidData(true);
+        return;
+      }
+      else {
+        setIsSubmitInvalidData(false);
+        await axios.post("https://wayi.league-funny.com/api/task", {
+          name: name,
+          description: description,
+          isCompleted: false
+        });
+        fetchData();
+      }
+    } catch (error) {
+      console.log("Error fetch:", error);
+    }
+  }
+
+  async function stateChangeHandler(task) {
+    try {
+      await axios.patch(`https://wayi.league-funny.com/api/task/${task.id}`);
+      await axios.put(`https://wayi.league-funny.com/api/task/${task.id}`, {
+        name: task.name,
+        description: task.description,
+        updated_at: new Date().toISOString()
+      });
+      fetchData();
+    } catch (error) {
+      console.log("Error fetch:", error);
+    }
+  }
+
+  async function deleteHandler(task) {
+    try {
+      await axios.delete(`https://wayi.league-funny.com/api/task/${task.id}`);
+      fetchData();
     } catch (error) {
       console.log("Error fetch:", error);
     }
@@ -41,22 +83,30 @@ export default function Home() {
     <main className={styles.main}>
       <form className={styles.form} onSubmit={(e) => submitHandler(e)}>
         <h1>My ToDo List</h1>
-        <div>
+        <div className={(isSubmitInvalidData && (name.length > 10 || name.length < 1)) ? styles.invalid : null}>
           <label htmlFor="nameInput">任務名稱</label>
-          <input id="nameInput" type="text" value={name} onChange={(e) => setName(e.target.value)}></input>
+          <input id="nameInput" type="text" value={name} maxLength={10} onChange={(e) => setName(e.target.value)}></input>
+          <p>必填 字數上限為10個字</p>
         </div>
-        <div>
+        <div className={(isSubmitInvalidData && description.length > 30) ? styles.invalid : null}>
           <label htmlFor="descriptionInput">任務描述</label>
           <textarea id="descriptionInput" type="textarea" value={description} rows={5} maxLength={30} onChange={(e) => setDescription(e.target.value)}></textarea>
+          <p>非必填 字數上限為30個字</p>
         </div>
+        <span className={styles.warning}>{isSubmitInvalidData ? "資料格式有誤 請修改內容後再送出" : null}</span>
         <button type="submit">新增任務</button>
       </form>
-
+      
       <ul className={styles.list}>
-        {currentData.map(task =>
+        <div className={styles.filter}>
+          <button onClick={() => showCompletedTaskHandler()}>{isHideCompletedTask ? "點擊顯示已完成任務" : "點擊隱藏已完成任務"}</button>
+        </div>
+        {currentData.filter(task => (isHideCompletedTask) ? !task.is_completed : task).map(task =>
           <li key={task.id}>
             <h1>{task.name}</h1>
-            <p>{task.description} {task.isCompleted ? " O " : " X "}</p>
+            <p>{task.description}</p>
+            <button className={`${styles.status} ${task.is_completed ? styles.completed : null}`} onClick={() => stateChangeHandler(task)}>{task.is_completed ? "任務已完成" : "任務未完成"}</button>
+            <button className={styles.delete} onClick={() => deleteHandler(task)}>刪除任務</button>
           </li>
         )
         }
